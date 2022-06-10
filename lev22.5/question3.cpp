98c0d6fc0a6d7372d463caa30915c0da24114693
zaoljkr <zaoljkr@gmail.com>
2022-06-10 22:10:15 +0900

    Create question3.cpp

---
+++ lev22.5/question3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char arr[3][3][4];
+	char input;
+	cin >> input;
+
+	for (int z = 0; z < 3; z++) {
+		for (int y = 0; y < 3; y++) {
+			for (int x = 0; x < 3; x++) {
+				arr[z][y][x] = input + z;
+			}
+		}
+	}
+
+	for (int z = 0; z < 3; z++) {
+		for (int y = 0; y < 3; y++) {
+			for (int x = 0; x < 3; x++) {
+				cout << arr[z][y][x];
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+
+
+	return 0;
+}
