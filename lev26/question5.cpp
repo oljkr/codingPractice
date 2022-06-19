1c31f989ee88459388fabfcb1be175ed6e29c78a
zaoljkr <zaoljkr@gmail.com>
2022-06-19 12:36:05 +0900

    Create question5.cpp

---
+++ lev26/question5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void swap(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+int main()
+{	
+	int a, b;
+	cin >> a >> b;
+	swap(&a, &b);
+	cout << a << " " << b;
+
+	return 0;
+}
