5ef8aaab874b0749ed7339dad3772687cc2408d0
zaoljkr <zaoljkr@gmail.com>
2022-06-22 01:11:23 +0900

    Create question12.cpp

---
+++ lev27o/question12.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string name[5] = {
+	"Amy","Bob","Chloe","Edger","Diane"
+};
+int map[5][5] = {
+	0,0,0,1,0,
+	1,0,0,0,0,
+	0,1,0,0,0,
+	0,0,0,0,0,
+	0,1,0,0,0,
+};
+
+int main()
+{
+	int cnt = 0;
+	int max = -99, maxIndex = -1;
+	for (int x = 0; x < 5; x++) {
+		cnt = 0;
+		for (int y = 0; y < 5; y++) {
+			if (map[y][x] == 1) cnt++;
+		}
+		if (cnt > max) {
+			max = cnt;
+			maxIndex = x;
+		}
+	}
+	cout << name[maxIndex];
+	return 0;
+}
